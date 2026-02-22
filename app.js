a2137d0eaf1c58af811fe9bf44f240dadf5786e7
CHAUHAN VAISHALI SURESHBHAI <vaishali@CHAUHANs-MacBook-Air.local>
2026-02-22 11:03:09 +0530

    done

--- app.js
+++ app.js
@@ -1,91 +1,171 @@
 
 import express from "express";
-import { fileURLToPath } from "url";
-import path from "path";
 import HttpError from "./middleware/HttpError.js";
 
 const app = express();
 const port = 5001;
 
-// Set EJS as the view engine
-app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-const __fileName = fileURLToPath(import.meta.url);
-const __dirName = path.dirname(__fileName);
-
-console.log("filename", __fileName);
-console.log("folder", __dirName);
-app.use(express.static(path.join(__dirName, "public")))
-
-let tasks = [
-    { id: 1, title: "Learn Node.js", completed: false },
-    { id: 2, title: "Master Express", completed: true }
+let todoList = [
+        {
+    id: 1,
+    title: "Drink Water",
+    description: "Drink at least 8 glasses of water today"
+    },
+    {
+    id: 2,
+    title: "Meditation",
+    description: "Meditate for 10 minutes to relax the mind"
+    },
+    {
+    id: 3,
+    title: "Plan the Day",
+    description: "Write down top 3 priorities for today"
+    },
+    {
+    id: 4,
+    title: "Revise Notes",
+    description: "Revise yesterday's study notes"
+    },
+    {
+    id: 5,
+    title: "Practice HTML & CSS",
+    description: "Create a small layout using flexbox"
+    },
+    {
+    id: 6,
+    title: "Update Resume",
+    description: "Add recent projects and skills"
+    },
+    {
+    id: 6,
+    title: "Check Emails",
+    description: "Reply to important emails"
+    }
 ];
 
 // Home Page
 app.get("/", (req, res) => {
-    const completed = tasks.filter(t => t.completed).length;
-    const uncompleted = tasks.length - completed;
-
-    res.render("index", { tasks, completed, uncompleted });
+    res.status(200).json({ message: "Welcome to To-Do List ✅" });
 });
 
-// Add Task
-app.post("/add", (req, res) => {
-    const newTask = {
+// get all to do list
+app.post("/todos", (req, res) => {
+    res.status(200).json({
+    message: "Todo list retrieved successfully!!!",todoList,});
+
+});
+
+// get to do by id
+app.get("/todos/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const todo = todoList.find((t) => t.id === id);
+
+    if (!todo) {
+        return res.status(404).json("To-do item not found");
+    }
+    res.status(200).json(todo);
+});
+//create to do 
+app.post("/createTodo", (req, res, next) => {
+
+    const { title, description } = req.body;
+
+    const newTodo = {
         id: new Date().getTime(),
-        title: req.body.task,
-        completed: false
+        title,
+        description,    
     };
 
-    tasks.push(newTask);
-    res.redirect("/");
+    todoList.push(newTodo);
+
+    res.status(201).json({
+        message: "To do item created successfully!!!",
+        newTodo,
+    });
+
+});
+//update to do using patch
+app.patch("/todos/:id", (req, res) => {
+
+    const id = Number(req.params.id);
+
+    const { task, description } = req.body;
+
+    const todo = todoList.find((t) => t.id === id);
+
+    if (!todo) {
+        return res.status(404).json({
+        message: "To do item not found",
+        });
+    }
+
+    if (task !== undefined) {
+    todo.task = task;
+    }
+
+    if (description !== undefined) {
+    todo.description = description;
+    }
+
+    res.status(200).json({ 
+        message: "To do item updated successfully",
+        todo,
+    });
 
 });
 
-// Toggle Complete
-app.post("/toggle/:id", (req, res) => {
+// replace to do using put
+
+app.put("/todos/:id", (req, res) => {
+
     const id = Number(req.params.id);
 
-    tasks = tasks.map(task => task.id === id
-        ? { ...task, completed: !task.completed } : task);
+    const { task, description } = req.body;
 
-    res.redirect("/");
-});
+    const index = todoList.findIndex((t) => t.id === id);
 
-// Delete Task
-app.post("/delete/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const index = tasks.findIndex((t) => t.id === id);
     if (index === -1) {
-        return res.status(404);
+        return res.status(404).json({
+        message: "To do item not found",
+        });
     }
-    tasks.splice(index, 1);
-    res.redirect("/");
+
+    todoList[index] = {
+        ...todoList[index],
+        task,
+        description,
+    };
+
+    res.status(200).json({
+        message: "To do item updated successfully",
+        todo: todoList[index],
+    });
+
+});
+//delete to do
+app.delete("/todos/:id", (req, res, next) => {
+    const id = parseInt(req.params.id);
+    const index = todoList.findIndex((t) => t.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({
+        message: "To do item not found",
+        });
+    }
+
+    todoList.splice(index, 1);
+
+    res.status(200).json({
+        message: "To do item deleted successfully"
+    });
 });
 
-// Edit Task
-app.post("/edit/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const updatedText = req.body.updatedTask;
-
-    if (!updatedText) {
-        return next(new HttpError("Updated task cannot be empty", 400));
-    }
-    const task = tasks.find(t => t.id === id);
-    if (!task) {
-        return next(new HttpError("Task not found", 404));
-    }
-    task.title = updatedText;
-    res.redirect("/");
-});
-
+//undefined route handling
 app.use((req, res, next) => {
     next(new HttpError("requested route not found", 404));
 });
-
 // centralize error handling
 
 app.use((error, req, res, next) => {
